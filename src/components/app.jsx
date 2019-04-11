b5b72e633364cd5134d5afdd34532755365db785
quentin roux <quentin.roux@hotmail.fr>
2019-04-11 17:49:13 +0200

    redux

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import CityList from '../containers/city_list';
+import City from '../containers/city';
 
+// TODO: remove this line and use Redux
 const App = () => {
   return (
     <div className="app">
-      <p>React + Redux starter</p>
+      <CityList />
+      <City />
     </div>
   );
 };
